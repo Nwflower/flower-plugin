3e97a4962d7dd69c59c09d1aeb862a44c51c09e6
西北一枝花 <1679659@qq.com>
2022-09-11 23:58:07 +0800

    完善屏蔽词功能

--- apps/wordListener.js
+++ apps/wordListener.js
@@ -3,15 +3,11 @@ import fs from 'fs'
 import YAML from 'yaml'
 import lodash from 'lodash'
 
-let pathk = process.cwd().replace(/\\/g, '/') + '/plugins/flower-plugin/resources/blackword/'
-if (!fs.existsSync(pathk)) {
-  fs.mkdirSync(pathk)
-}
 export class wordListener extends plugin {
   constructor () {
     let rule = {
       /** 命令正则匹配 */
-      reg: '.*',
+      reg: '^[^(解除|删除|取消|屏蔽)]+',
       /** 执行方法 */
       fnc: 'wordsListener'
     }
@@ -22,128 +18,218 @@ export class wordListener extends plugin {
       event: 'message.group',
       priority: 9876,
       rule: [rule, {
-        reg: '#*解除屏蔽(.)+',
+        reg: '^#*(解除|删除|取消|不)屏蔽(本群|全局)?(.)+',
         /** 执行方法 */
         fnc: 'delBlackWord'
       }, {
-        reg: '#*添加屏蔽(.)+',
+        reg: '^#*屏蔽(本群|全局)?[^(词列表)](.)*',
         /** 执行方法 */
         fnc: 'addBlackWord'
+      }, {
+        reg: '^#*屏蔽词列表',
+        fnc: 'blackList'
       }]
     })
     this._path = process.cwd().replace(/\\/g, '/')
+    this.wordResPath = `${this._path}/plugins/flower-plugin/resources/blackword`
     this.islog = false
     Object.defineProperty(rule, 'log', {
       get: () => this.islog
     })
   }
 
-  async wordsListener (e) {
-    if (this.e.isMaster) { return false }
+  async init (path = `${this.wordResPath}/`) {
+    if (!fs.existsSync(path)) { fs.mkdirSync(path) }
+  }
+
+  async blackList () {
+    let words = []
+    let files = []
+    let forWordMsg = []
+    let globalPath = `${this.wordResPath}/global`
+    let groupPath = `${this.wordResPath}/${this.e.group_id}`
+    // 获取本群违禁词
+    if (fs.existsSync(groupPath)) {
+      files = fs.readdirSync(groupPath).filter((file) => file.endsWith('.yaml'))
+      for (let file of files) { words = lodash.unionWith(YAML.parse(fs.readFileSync(`${groupPath}/${file}`, 'utf8')), words) }
+    }
+    forWordMsg.push({
+      message: `本群屏蔽词如下，共 ${words.length} 个`,
+      nickname: Bot.nickname,
+      user_id: Bot.uin
+    })
+    for (let i = 0; i < words.length; i += 100) {
+      let message = []
+      for (let j = i; j < i + 100 && j < words.length; j++) {
+        message.push(`${j + 1}、【${words[j]}】`)
+        if (j !== words.length - 1) message.push('\n')
+      }
+      forWordMsg.push({
+        message,
+        nickname: Bot.nickname,
+        user_id: Bot.uin
+      })
+      if (i > 4800) {
+        forWordMsg.push({
+          message: `本群屏蔽词较多，剩下的${words.length - 4800}个词语已经省略`,
+          nickname: Bot.nickname,
+          user_id: Bot.uin
+        })
+        break
+      }
+    }
+
+    // 获取全局违禁词
+    words = []
+    files = fs.readdirSync(globalPath).filter((file) => file.endsWith('.yaml'))
+    for (let file of files) { words = lodash.unionWith(YAML.parse(fs.readFileSync(`${globalPath}/${file}`, 'utf8')), words) }
+    forWordMsg.push({
+      message: `全局屏蔽词如下，共 ${words.length} 个`,
+      nickname: Bot.nickname,
+      user_id: Bot.uin
+    })
+    for (let i = 0; i < words.length; i += 100) {
+      let message = []
+      for (let j = i; j < i + 100 && j < words.length; j++) {
+        message.push(`${j + 1}、【${words[j]}】`)
+        if (j !== words.length - 1) message.push('\n')
+      }
+      forWordMsg.push({
+        message,
+        nickname: Bot.nickname,
+        user_id: Bot.uin
+      })
+      if (i > 4800) {
+        forWordMsg.push({
+          message: `全局屏蔽词较多，剩下的${words.length - 4800}个词语已经省略`,
+          nickname: Bot.nickname,
+          user_id: Bot.uin
+        })
+      }
+    }
+    let sed = await this.e.group.makeForwardMsg(forWordMsg)
+    await this.reply(sed)
+  }
+
+  async wordsListener () {
+    // if (this.e.isMaster) { return false }
     if (!this.e.group.is_admin && !this.e.group.is_owner) { return false }
-    let msgdata = ''
-    for (let val of e.message) {
+    let receivedMsg = ''
+    for (let val of this.e.message) {
       switch (val.type) {
         case 'text':
-          msgdata = msgdata + val.text
+          receivedMsg = receivedMsg + val.text
           break
         default:
           break
       }
     }
-    if (msgdata !== '') {
-      return this.wordListener(msgdata)
-    }
-    return false
-  }
-
-  async CheckAuth () {
-    let qq = this.e.group.pickMember(this.e.sender.user_id)
-    if (qq.is_owner || qq.is_admin || qq.isMaster) {
-      return true
-    }
-    return false
-  }
-
-  async getBlackWords () {
-    let wordlist = []
-    const worldfiles = fs
-      .readdirSync(`${this._path}/plugins/flower-plugin/resources/blackword`)
-    for (let file of worldfiles) {
-      wordlist = lodash.unionWith(YAML.parse(
-        fs.readFileSync(`${this._path}/plugins/flower-plugin/resources/blackword/${file}`, 'utf8')), wordlist)
-    }
-    return wordlist
-  }
-
-  async delBlackWord () {
-    if (!this.e.group.is_admin && !this.e.group.is_owner && !this.e.isMaster) { return false }
-    if (!await this.CheckAuth()) {
-      this.reply('很抱歉，你没有权限')
-    }
-    let word = this.e.msg.replaceAll(/#*解除屏蔽/g, '').trim()
-    const worldfiles = fs
-      .readdirSync(`${this._path}/plugins/flower-plugin/resources/blackword`)
-    let flag = false
-    for (let file of worldfiles) {
-      let wordlist = YAML.parse(fs.readFileSync(`${this._path}/plugins/flower-plugin/resources/blackword/${file}`, 'utf8'))
-      if (wordlist.includes(word)) {
-        flag = true
-        wordlist = lodash.remove(wordlist, function (aword) {
-          return aword !== word
-        })
-        fs.writeFileSync(`${this._path}/plugins/flower-plugin/resources/blackword/${file}`, YAML.stringify(wordlist, null, '\t'))
-      }
-    }
-    if (flag) {
-      this.reply(`取消屏蔽疑似违规词汇：【${word}】`, true)
-    } else {
-      this.reply(`没有找到违规词汇：【${word}】`, true, 110)
-    }
-    return true
-  }
-
-  async addBlackWord () {
-    if (!this.e.group.is_admin && !this.e.group.is_owner && !this.e.isMaster) { return false }
-    if (!await this.CheckAuth()) {
-      this.reply('很抱歉，你没有权限')
-    }
-    let word = this.e.msg.replaceAll(/#*添加屏蔽/g, '').trim()
-    if (!word) { return false }
-    let exword = []
-    let defpath = `${this._path}/plugins/flower-plugin/resources/blackword/def.yaml`
-    if (fs.existsSync(defpath)) {
-      exword = YAML.parse(fs.readFileSync(defpath, 'utf8'))
-    }
-    if (exword.includes(word)) {
-      this.reply('已经添加过该词!')
-      return true
-    } else {
-      exword.push(word)
-      fs.writeFileSync(defpath, YAML.stringify(exword, null, '\t'))
-      this.reply(`新增屏蔽违规词汇：【${word}】`, true, 20)
-    }
-    return true
-  }
-
-  async wordListener (msgdata) {
-    try {
-      let DelReg = /#*解除屏蔽/g
+    if (receivedMsg !== '') {
+      let DelReg = /#*(解除|删除|取消|不)屏蔽(本群|全局)?/g
       if (DelReg.test(this.e.msg)) { return false }
       let wordlist = await this.getBlackWords()
       for (let word of wordlist) {
-        if (msgdata.includes(word) && word) {
+        if (receivedMsg.includes(word) && word) {
           this.e.group.recallMsg(this.e.message_id)
           await this.e.group.muteMember(this.e.sender.user_id, 1)
-          this.reply('检测到违规词汇，已经制裁')
+          await this.reply('检测到违规词汇，已经制裁')
           logger.info(`检测到违禁词：${word}`)
           this.islog = true
           return true
         }
       }
-    } catch (e) {
-      logger.info('【违规词监听】监听未能正常运行')
     }
     return false
+  }
+
+  // 检查权限
+  async CheckAuth () {
+    let qq = this.e.group.pickMember(this.e.sender.user_id)
+    return (qq.is_owner || qq.is_admin || this.e.isMaster)
+  }
+
+  // 获得违禁词
+  async getBlackWords () {
+    let words = []
+    let globalPath = `${this.wordResPath}/global`
+    let groupPath = `${this.wordResPath}/${this.e.group_id}`
+    // 获取全局违禁词
+    let files = fs.readdirSync(globalPath).filter((file) => file.endsWith('.yaml'))
+    for (let file of files) { words = lodash.unionWith(YAML.parse(fs.readFileSync(`${globalPath}/${file}`, 'utf8')), words) }
+    // 获取本群违禁词
+    if (fs.existsSync(groupPath)) {
+      files = fs.readdirSync(groupPath).filter((file) => file.endsWith('.yaml'))
+      for (let file of files) { words = lodash.unionWith(YAML.parse(fs.readFileSync(`${groupPath}/${file}`, 'utf8')), words) }
+    }
+    return words
+  }
+
+  async delBlackWord () {
+    if (!this.e.group.is_admin && !this.e.group.is_owner) { return false }
+    if (!await this.CheckAuth()) {
+      this.reply('很抱歉，你没有权限')
+      return true
+    }
+    let word = this.e.msg.replaceAll(/#*(解除|删除|取消|不)屏蔽(本群|全局)?/g, '').trim()
+    if (!word) { return false }
+    let folderPath = `${this.wordResPath}/global`
+    let isLocal = /^#*(解除|删除|取消|不)屏蔽本群/g.test(this.e.msg)
+    if (isLocal) { folderPath = `${this.wordResPath}/${this.e.group_id}` }
+    const files = fs.readdirSync(folderPath).filter((file) => file.endsWith('.yaml'))
+    let flag = false
+    for (let file of files) {
+      let wordlist = YAML.parse(fs.readFileSync(`${folderPath}/${file}`, 'utf8'))
+      if (wordlist.includes(word)) {
+        flag = true
+        wordlist = lodash.remove(wordlist, (thisWord) => thisWord !== word)
+        if (!wordlist) {
+          fs.unlinkSync(`${folderPath}/${file}`)
+          continue
+        }
+        fs.writeFileSync(`${folderPath}/${file}`, YAML.stringify(wordlist, null, '\t'))
+      }
+    }
+    if (flag) {
+      let reMsg = (isLocal) ? `删除了本群屏蔽词：【${word}】` : `删除了全局屏蔽词：【${word}】`
+      this.reply(reMsg, true)
+    } else {
+      this.reply(`该词没有被屏蔽：【${word}】`, true, 110)
+    }
+    return true
+  }
+
+  async addBlackWord () {
+    if (!this.e.group.is_admin && !this.e.group.is_owner) { return false }
+    if (!await this.CheckAuth()) {
+      this.reply('很抱歉，你没有权限')
+      return true
+    }
+    let handleSentence = this.e.msg.replaceAll(/#*屏蔽(本群|全局)?/g, '').trim()
+    if (!handleSentence) { return false }
+    let handleWords = handleSentence.replaceAll('，', ',').split(',')
+    let indexWords = []
+    let existWord = []
+    let folderPath = `${this.wordResPath}/global/`
+    let isLocal = /^#*屏蔽本群/g.test(this.e.msg)
+    if (isLocal) { folderPath = `${this.wordResPath}/${this.e.group_id}/` }
+    await this.init(folderPath)
+    let wordPath = `${folderPath}${await this.getData()}.yaml`
+
+    if (fs.existsSync(wordPath)) { indexWords = YAML.parse(fs.readFileSync(wordPath, 'utf8')) }
+    for (let handleWord of handleWords) { if (indexWords.includes(handleWord)) { existWord.push(handleWord) } else { indexWords.push(handleWord) } }
+    fs.writeFileSync(wordPath, YAML.stringify(indexWords, null, '\t'))
+    let reMsg = (isLocal) ? `新增本群屏蔽词：【${handleWords.join('、')}】` : `新增全局屏蔽词：【${handleWords.join('】【')}】`
+    if (existWord.length) {
+      reMsg = reMsg + `\n词语【${existWord.join('】【')}】已经被屏蔽`
+    }
+    this.reply(reMsg, true, 20)
+    return true
+  }
+
+  async getData () {
+    let today = new Date()
+    let month = today.getMonth() + 1
+    month = (month > 9) ? month : ('0' + month)
+    return `${today.getFullYear()}-${month}`
   }
 }
